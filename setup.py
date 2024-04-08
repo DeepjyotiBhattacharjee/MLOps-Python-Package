aaec2c4e77c0dcef7aeba6d1dae149d2b325df62
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-04-08 23:33:30 +0530

    added setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup, find_packages
 from typing import List
 
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str) -> List[str]:
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -25,4 +37,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = get_requirements("./requirements_dev.txt")
     )
